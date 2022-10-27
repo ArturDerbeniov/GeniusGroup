b7815febdd8b9af6e3bb53f747ddb740b84f3655
ArturDerbeniov <74465913+ArturDerbeniov@users.noreply.github.com>
2022-10-27 17:48:27 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -19,6 +19,9 @@ function WindowLoad() {
 			});
 		});
 	}
+
+	logosRunningLines();
+	orbitaPills();
 }
 function eventDocClick(e) {
     var targ = e.target;
@@ -101,6 +104,16 @@ var initSlick = {
 		}
 	},
 	startCards: function() {
+		var slickWrapper;
+		$(".cards-slick").on("init", function(event, slick) {
+			slickWrapper = slick.$list[0].closest(".cards-slickWrapper");
+			if(slickWrapper.classList.contains("cardBlog-slickWrapper")) {
+				initCardBlogSlickWrapper(slickWrapper.clientHeight);
+			}
+			else if(slickWrapper.classList.contains("cardCalendar-slickWrapper")) {
+				initCardCalendarSlickWrapper(slickWrapper.clientHeight);
+			}
+		});
 		$(".cards-slick").slick(initSlick.cardsSlickParams_1);
 	},
 	startGallery: function() {
@@ -194,4 +207,294 @@ var initSlick = {
 	        }
 	    ]
 	}	
+};
+
+gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
+
+function initCardBlogSlickWrapper(hBlocg) {
+	const first = gsap.timeline({});
+	first.set(".cardBlog-slickWrapper .cards-slick", {
+		height:hBlocg,
+		onComplete: cardBlog
+	});
+
+	function cardBlog() {
+		var cardBlogSlickWrapper = document.querySelector(".cardBlog-slickWrapper");
+		cardBlogSlickWrapper.style.height = hBlocg + "px";
+		cardBlogSlickWrapper.classList.add("waitToAnimate");
+
+		gsap.to(".cardBlog-slickWrapper", {
+			scrollTrigger: {
+			    trigger: ".cardBlog-slickWrapper",
+			    start: "top 80%",
+			    end: "bottom 0",
+			    markers: false,
+			    onEnter: ({progerss, direction, isActive}) => {
+			    	cardBlogSlickWrapper.classList.remove("waitToAnimate");
+			    	setTimeout(function() {
+			    		cardBlogSlickWrapper.classList.remove("waitToAnimate");
+			    		cardBlogSlickWrapper.classList.add("animationDone");
+			    	}, 1000);
+			    	// console.log("onEnter: ", progerss, direction, isActive);
+			    },
+			    onEnterBack: ({progerss, direction, isActive}) => {
+			    	// document.querySelector(".cardBlog-slickWrapperForAnimate").classList.remove("waitToAnimate");
+			    	// console.log("onEnterBack: ", progerss, direction, isActive);
+			    },
+			    onLeave: ({progerss, direction, isActive}) => {
+			    	// document.querySelector(".cardBlog-slickWrapperForAnimate").classList.add("waitToAnimate");
+			    	// console.log("onLeave: ", progerss, direction, isActive);
+			    },
+			    onLeaveBack: ({progerss, direction, isActive}) => {
+			    	cardBlogSlickWrapper.classList.add("waitToAnimate");
+			    	cardBlogSlickWrapper.classList.remove("animationDone");
+			    	// console.log("onLeaveBack: ", progerss, direction, isActive);
+			    }
+			    // onUpdate: ({progerss, direction, isActive}) => console.log("onUpdate: ", progerss, direction, isActive)
+			}
+		})
+	};
 }
+function initCardCalendarSlickWrapper(hCalendar) {
+	const first = gsap.timeline({});
+
+	first.set(".cardCalendar-slickWrapper .cards-slick", {
+		height:hCalendar,
+		onComplete: cardCalendar
+	});
+
+	function cardCalendar() {
+		var calendarBlogSlickWrapper = document.querySelector(".cardCalendar-slickWrapper");
+		calendarBlogSlickWrapper.style.height = hCalendar + "px";
+		calendarBlogSlickWrapper.classList.add("waitToAnimate");
+
+		gsap.to(".cardCalendar-slickWrapper", {
+			scrollTrigger: {
+			    trigger: ".cardCalendar-slickWrapper",
+			    start: "top 80%",
+			    end: "bottom 0",
+			    markers: false,
+			    onEnter: ({progerss, direction, isActive}) => {
+			    	calendarBlogSlickWrapper.classList.remove("waitToAnimate");
+			    	setTimeout(function() {
+			    		calendarBlogSlickWrapper.classList.remove("waitToAnimate");
+			    		calendarBlogSlickWrapper.classList.add("animationDone");
+			    	}, 1000);
+			    	// console.log("onEnter: ", progerss, direction, isActive);
+			    },
+			    onEnterBack: ({progerss, direction, isActive}) => {
+			    	// document.querySelector(".cardCalendar-slickWrapperForAnimate").classList.remove("waitToAnimate");
+			    	// console.log("onEnterBack: ", progerss, direction, isActive);
+			    },
+			    onLeave: ({progerss, direction, isActive}) => {
+			    	// document.querySelector(".cardCalendar-slickWrapperForAnimate").classList.add("waitToAnimate");
+			    	// console.log("onLeave: ", progerss, direction, isActive);
+			    },
+			    onLeaveBack: ({progerss, direction, isActive}) => {
+			    	calendarBlogSlickWrapper.classList.add("waitToAnimate");
+			    	calendarBlogSlickWrapper.classList.remove("animationDone");
+			    	// console.log("onLeaveBack: ", progerss, direction, isActive);
+			    }
+			    // onUpdate: ({progerss, direction, isActive}) => console.log("onUpdate: ", progerss, direction, isActive)
+			}
+		})
+	}
+}
+
+function logosRunningLines() {
+	var logosWrapper;
+	if(logosWrapper = document.querySelector(".logos")) {
+		var lines = logosWrapper.querySelectorAll(".logos__line");
+		if(lines.length) {
+			lines.forEach((line) => {
+				line.style.width = line.parentNode.clientWidth + "px";
+				initGsap(line, line.getAttribute("data-direction"), line.querySelectorAll(".logos__line__item").length, line.getAttribute("data-speed"));
+			});
+		}
+	}
+
+	function initGsap(line, direction, itemsQ, speed) {
+		var lineClsStr = "." + line.classList[1],
+			itemsClsStr = lineClsStr + " .logos__line__item",
+			sign = direction == 1 ? '+' : '-',
+			lineSize = (itemsQ * 205) + (itemsQ * 15);
+
+		gsap.set(itemsClsStr, {
+		  x: (i) => i * (220 * direction)
+		});
+
+		const gsapTween = gsap.to(itemsClsStr, {
+			duration: speed,
+			ease: "linear",
+			x: sign+"="+lineSize+"px",
+			scrollTrigger: {
+				trigger: ".logos",
+			    start: "top 80%",
+			    end: "bottom 50px",
+			    markers: false,
+			    onEnter: () => {
+			    	document.querySelector(".logos").classList.add("active");			    	
+			    },
+			    onLeave: () => {
+			    	gsapTween.pause();
+			    },
+			    onEnterBack: () => {
+			    	gsapTween.resume();
+			    },
+			    onLeaveBack: () => {
+			    	gsapTween.pause();
+			    }
+			},
+			modifiers: {
+				x: gsap.utils.unitize(x => (parseFloat(x) % lineSize))
+			},
+			repeat: -1
+		});
+
+		document.querySelector(lineClsStr).addEventListener("mouseenter", function () {
+		  gsap.to(gsapTween, { timeScale: 0.3});
+		});
+
+		document.querySelector(lineClsStr).addEventListener("mouseleave", function () {
+		  gsap.to(gsapTween, { timeScale: 1});
+		});
+	}
+}
+
+function orbitaPills() {	
+	var planetoidWrapper;
+
+	if(planetoidWrapper = document.querySelector(".planetoid")) {
+		var orbites = planetoidWrapper.querySelectorAll(".planetoid__orbita");
+		if(orbites.length) {
+			orbites.forEach((orbita) => {
+				initOrbita(orbita, orbita.querySelectorAll(".planetoid__pill").length);
+			});
+		}
+	}
+
+	function initOrbita(orbita, pillsQ) {
+		var orbitTime = 20,
+			orbitPills = pillsQ,
+			orbitTiming = orbitTime / pillsQ,
+			pills = orbita.querySelectorAll(".planetoid__pill"),
+			isFirstInit = true,
+			arrayPills = [],
+			orbita_num = pills[0].getAttribute("data-orbita"),
+			isFirstTimeAddEvent = true;
+
+
+		const orbitaTween = gsap.to(orbita, {
+			duration: orbitTime,
+			ease: "linear",
+			scrollTrigger: {
+				trigger: ".planetoid",
+			    start: "top 80%",
+			    end: "bottom 50px",
+			    markers: false,
+			    onEnter: () => {
+			    	if(isFirstInit) {
+			    		isFirstInit = false;
+
+			    		pills.forEach((pill, i) => {
+							var orbitStartTime,
+								orbita_num = pill.getAttribute("data-orbita");							
+
+							orbitStartTime = orbitTiming * i;
+
+							gsap.to(pill, {
+								duration: orbitTime, 
+								repeat: -1,
+								ease: "linear",
+								motionPath:{
+									path: "#orbita_"+orbita_num,
+									align: "#orbita_"+orbita_num,
+									autoRotate: false,
+									alignOrigin: [0.5, 0.5],
+								}
+							},orbitStartTime);
+
+							arrayPills.push(pill);
+							pill.classList.add("showup");
+						}); // /forEach
+
+			    	}
+			    	// document.querySelector(".logos").classList.add("active");			    	
+			    },
+			    onLeave: () => {
+			    	orbitaTween.pause();
+			    },
+			    onEnterBack: () => {
+			    	orbitaTween.resume();
+			    },
+			    onLeaveBack: () => {
+			    	orbitaTween.pause();
+			    }
+			}
+		});
+
+		if(isFirstTimeAddEvent) {
+			isFirstTimeAddEvent = false;
+
+			document.querySelector(".planetoid__orbita-"+orbita_num).addEventListener("mouseenter", function () {
+			  gsap.to(orbitaTween, { timeScale: 0.3});
+			});
+
+			document.querySelector(".planetoid__orbita-"+orbita_num).addEventListener("mouseleave", function () {
+			  gsap.to(orbitaTween, { timeScale: 1});
+			});		
+		}
+	}
+}
+/*
+(function() {
+	var orbitTime = 20,
+		orbitPills = 5,
+		orbitTiming = orbitTime / orbitPills,
+		pills = document.querySelectorAll(".planetoid .planetoid__pill");
+
+	// var tl = gsap.timeline({repeat: -1,   defaults: { duration: orbitTime, ease: "none" }});
+	var globCountPills = 0;
+	var arrayPills = [];
+
+	pills.forEach((pill, i) => {
+		var orbitStartTime,
+			orbita_num = pill.getAttribute("data-orbita");
+
+		globCountPills = i;
+		if(globCountPills >= 4) {
+			i = i - 5;
+		}
+
+		orbitStartTime = orbitTiming * i;
+
+
+		gsap.to(pill, {
+			duration: orbitTime, 
+			repeat: -1,
+			ease: "linear",
+			motionPath:{
+				path: "#orbita_"+orbita_num,
+				align: "#orbita_"+orbita_num,
+				autoRotate: false,
+				alignOrigin: [0.5, 0.5],
+			}
+		},orbitStartTime);
+
+		arrayPills.push(pill);
+
+
+	}); // /forEach
+		
+
+		for(let i = 0; i < arrayPills.length; i++ ) {
+			let timing = 4000;
+			(function (i, t) {
+				setTimeout(function() {					
+					arrayPills[i].classList.add("showup");
+				}, t*i);
+			})(i, timing);
+		}
+
+})();
+*/
